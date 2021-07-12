f665ce88484fe3592923fe01f6574fed606118ac
CeleRn <celern84@gmail.com>
2021-07-12 20:08:16 +0300

    Add DocViewer

--- frontend/src/components/DownloadsList.js
+++ frontend/src/components/DownloadsList.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Container from '@material-ui/core/Container';
+// import Container from '@material-ui/core/Container';
+
+import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+
 import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,7 +14,18 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+
+
 import GetAppIcon from '@material-ui/icons/GetApp';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,21 +38,44 @@ const useStyles = makeStyles(theme => ({
     fontWeight: 700,
     fontSize: '1.2em'
   },
+  modalBody: {
+    marginTop: 80,
+    height: "100%"
+  }
 }),
 {
   name: 'DownloadsList'
 });
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const DownloadsList = ({downloadsList}) => {
   const classes = useStyles();
   // console.log(rows);
+
+  const [openModal, setOpenModal] = useState(false);
+  const [doc, setDoc] = useState({uri:""});
+
+
+  const handleViewClick = doc => () => {
+    setOpenModal(true)
+    setDoc({title:doc.title,link:doc.link})
+  }
+  
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
   return (
-      <TableContainer component={Paper} className={classes.root}>
+    <>
+    <TableContainer component={Paper} className={classes.root}>
         <Table className={classes.table} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell className={classes.head}>Документ</TableCell>
+              <TableCell className={classes.head} align="center">Открыть</TableCell>
               <TableCell className={classes.head} align="right">Скачать</TableCell>
             </TableRow>
           </TableHead>
@@ -47,6 +84,11 @@ const DownloadsList = ({downloadsList}) => {
               <TableRow key={row.title}>
                 <TableCell component="th" scope="row">
                   {row.title}
+                </TableCell>
+                <TableCell align="center">
+                  <IconButton onClick={handleViewClick(row)}>
+                    <VisibilityIcon/>
+                  </IconButton>
                 </TableCell>
                 <TableCell align="right">
                   <IconButton href={row.link} target="_blank">
@@ -58,6 +100,24 @@ const DownloadsList = ({downloadsList}) => {
           </TableBody>
         </Table>
       </TableContainer>
+      <Dialog fullScreen open={openModal} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              {doc.title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.modalBody}>
+          <DocViewer pluginRenderers={DocViewerRenderers} documents={[{uri: doc.link}]} style={{height: "100%"}} />
+        </div>
+        
+      </Dialog>
+    </>
+      
   )
 }
 
